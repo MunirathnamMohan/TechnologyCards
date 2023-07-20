6ae71fb606949192934bde1cc567f017a1436573
Mohan98765@ <Mohan765>
2023-07-20 06:46:47 +0000

    TechnologyCards

---
+++ src/components/CardItem/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+const ConstItem = props => {
+  const {card} = props
+  const {title, description, imgUrl, className} = card
+  return (
+    <li className={className}>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <div>
+        <img src={imgUrl} alt={title} />
+      </div>
+    </li>
+  )
+}
+
+export default ConstItem
